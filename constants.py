9289ea3372dbf7783152e3613109c760112be13d
LuizWillner <luizwillner@gmail.com>
2025-07-07 01:57:15 -0300

    Primeiro commit

---
+++ constants.py
@@ -0,0 +1,14 @@
+import os
+
+
+DIR_GAMES_DATA = "data/games/"
+DIR_MOVIES_SHOW_DATA = "data/movies-shows/"
+DIR_OTHERS_DATA = "data/others/"
+DIR_OUTPUT_BASE = "data/output/"
+DIR_OUTPUT_TABLES = os.path.join(DIR_OUTPUT_BASE, "tables/")
+DIR_OUTPUT_GRAPHS = os.path.join(DIR_OUTPUT_BASE, "graphs/")
+
+GAME_ADAPTATION_FILE_NAME = 'game_adaptation.csv'
+MOVIES_FILE_NAME = 'game_movie_adaptation.csv'
+SHOWS_FILE_NAME = 'game_show_adaptation.csv'
+
